3eb5c2dda376a5ae05f72d1103bce8220c0f3d84
LoneBeast-hub <yusufoyindamola5@gmail.com>
2024-06-05 18:59:20 +0100

    did full name and email input on edit profile page

--- src/pages/edit_profile/edit_profile.page.jsx
+++ src/pages/edit_profile/edit_profile.page.jsx
@@ -14,7 +14,7 @@ const EditProfile = () => {
             <div className="w-[90%] mx-auto">
                 {/* heading */}
                 <p className="text-primary-100 mb-[2.1rem] font-bold text-[2rem] md:text-[3.2rem]">Personal Information</p>
-                <div className="border border-gray-e5 pt-[1.7rem] pb-[2.5rem] px-[2.1rem] md:px-[5.6rem] md:py-[2.4rem]">
+                <div className="border border-gray-e5 pt-[1.7rem] pb-[2.5rem] px-[5%] md:py-[2.4rem]">
                     {/* Profile picture */}
                     <div className="relative h-fit w-fit">
                         {/* img */}
@@ -27,11 +27,16 @@ const EditProfile = () => {
                     {/* form */}
                     <form className="mt-[14px] md:mt[17px]" action="#">
                         {/* flex */}
-                        <div className="flex flex-col md:flex-row">
+                        <div className="flex flex-col gap-[2rem] md:flex-row">
                             {/* input */}
-                            <div>
+                            <div className="flex w-full md:w-[50%] flex-col md:gap-[1.5rem] gap-[0.5rem]">
                                 <label htmlFor="full_name" className="text-[1.4rem] md:text-[2rem] text-black-100">Full Name</label>
-                                <input className="text-[2rem] text-black-100" type="text" name="full_name" id="full_name" placeholder="John Doe" />
+                                <input className="border border-gray-e5 text-[1.4rem] py-[2rem] rounded-[5px] px-[1.5rem] md:py-[2.5rem] md:px-[2rem] md:text-[2rem] text-black-100" type="text" name="full_name" id="full_name" placeholder="John Doe" />
+                            </div>
+                            {/* input */}
+                            <div className="flex w-full md:w-[50%] flex-col md:gap-[1.5rem] gap-[0.5rem]">
+                                <label htmlFor="email" className="text-[1.4rem] md:text-[2rem] text-black-100">Email</label>
+                                <input className="border border-gray-e5 text-[1.4rem] py-[2rem] rounded-[5px] px-[1.5rem] md:py-[2.5rem] md:px-[2rem] md:text-[2rem] text-black-100" type="email" name="email" id="email" placeholder="example@gmail.com" />
                             </div>
                         </div>
                     </form>
