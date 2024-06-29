cf665466e120df338c5e4208236ea97cd790dfba
LoneBeast-hub <yusufoyindamola5@gmail.com>
2024-06-29 12:01:30 +0100

    re configured the route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://lod-goodness-gym.netlify.app/',
+  base: 'https://lod-goodness-gym.netlify.app',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }]
